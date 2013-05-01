c9f14f8a670f9b2f9315baa0ffb47e92a213a518
Alex Rudnick <alex.rudnick@gmail.com>
2013-05-01 00:40:09 -0400

    add cross-validation-like code to repl

--- hmmwsd/repl.py
+++ hmmwsd/repl.py
@@ -3,7 +3,9 @@
 
 import sys
 import argparse
+from argparse import Namespace
 import pickle
+import random
 
 import nltk
 
@@ -16,6 +18,8 @@ from util_run_experiment import output_five_best
 from util_run_experiment import all_target_languages
 from util_run_experiment import all_words
 from constants import BEAMWIDTH
+from constants import UNTRANSLATED
+import stanford
 
 def get_argparser():
     parser = argparse.ArgumentParser(description='repl')
@@ -23,7 +27,80 @@ def get_argparser():
     parser.add_argument('--targetlang', type=str, required=True)
     parser.add_argument('--treetaggerhome', type=str, required=False,
                         default="../TreeTagger/cmd")
+    parser.add_argument('--randomvalidate', type=bool, required=False,
+                        default=True)
     return parser
+
+def repl(model, lm, emissions, cfd):
+    while True:
+        try:
+            line = input('>')
+        except: break
+        line = line.strip()
+        sentences = nltk.sent_tokenize(line)
+        s_tokenized = [nltk.word_tokenize(sent) for sent in sentences]
+        tokenized = []
+        for sent in s_tokenized: tokenized.extend(sent)
+
+        tagger = stanford.get_tagger()
+        postags = [t.lower() for (w,t) in tagger.tag(tokenized)]
+
+        sss = learn.maybe_lemmatize([tokenized], 'en', tt_home)
+        lemmas = sss[0]
+        ss = list(map(nltk.tag.tuple2str, zip(lemmas,postags)))
+        print(" ".join(ss))
+
+        if model == "unigram":
+            tagged = skinnyhmm.mfs(cfd, ss)
+        if model == "bigram":
+            tagged = skinnyhmm.viterbi(lm, emissions, cfd, ss)
+        elif model == "trigram":
+            tagged = searches.beam(lm, emissions, cfd, ss, beamwidth=BEAMWIDTH)
+        print(tagged)
+
+def randomvalidate(model, lm, emissions, cfd):
+    args = Namespace()
+    args.targetlang="es"
+    args.sourcetext="/space/Europarl_Intersection_preprocessed/intersection.en.txt.ascii.taggedlemmas" 
+    args.targettext="/space/Europarl_Intersection_preprocessed/intersection.es.txt.lemmas"
+    args.alignments="/space/output_en_es/training.align"
+    args.fast=False
+
+    triple_sentences = learn.load_bitext(args)
+    tl_sentences = learn.get_target_language_sentences(triple_sentences)
+    sl_sentences = [source for (source,target,align) in triple_sentences]
+    sentence_pairs = list(zip(sl_sentences, tl_sentences))
+
+    totalcorrect = 0
+    totalwords = 0
+
+    for i in range(1000):
+        (ss, ts) = random.choice(sentence_pairs)
+        print(" ".join(ss))
+        if model == "unigram":
+            tagged = skinnyhmm.mfs(cfd, ss)
+        if model == "bigram":
+            tagged = skinnyhmm.viterbi(lm, emissions, cfd, ss)
+        elif model == "trigram":
+            tagged = searches.beam(lm, emissions, cfd, ss, beamwidth=BEAMWIDTH)
+
+        print("ORIGINAL:", list(zip(ss,ts)))
+        print("TAGGED:", tagged)
+
+        predicted = [t for (s,t) in tagged]
+        correct = 0
+        print(list(zip(ts, predicted)))
+        for actual, pred in zip(ts, predicted):
+            if actual == UNTRANSLATED: continue
+            totalwords += 1
+            if actual == pred:
+                correct += 1
+                totalcorrect += 1
+        print("sentence accuracy:", correct / len(ss))
+        print("considered words:", len(ss))
+    accuracy = (totalcorrect / totalwords)
+    print("accuracy:", accuracy)
+    print("considered words:", totalwords)
 
 def main():
     parser = get_argparser()
@@ -34,6 +111,7 @@ def main():
     targetlang = args.targetlang
     tt_home = args.treetaggerhome
     model = args.model
+    stanford.taggerhome = "/home/alex/software/stanford-postagger-2012-11-11"
 
     print("Loading models...")
     lm, emissions = None, None
@@ -50,24 +128,9 @@ def main():
     emissions = learn.cpd(emissions)
     print("OK loaded models.")
 
-    while True:
-        try:
-            line = input('>')
-        except: break
-        line = line.strip()
-        sentences = nltk.sent_tokenize(line)
-        s_tokenized = [nltk.word_tokenize(sent) for sent in sentences]
-        tokenized = []
-        for sent in s_tokenized: tokenized.extend(sent)
-        sss = learn.maybe_lemmatize([tokenized], 'en', tt_home)
-        ss = sss[0]
-        print(" ".join(ss))
-        if model == "unigram":
-            tagged = skinnyhmm.mfs(cfd, ss)
-        if model == "bigram":
-            tagged = skinnyhmm.viterbi(lm, emissions, cfd, ss)
-        elif model == "trigram":
-            tagged = searches.beam(lm, emissions, cfd, ss, beamwidth=BEAMWIDTH)
-        print(tagged)
+    if not args.randomvalidate:
+        repl(model, lm, emissions, cfd)
+    else:
+        randomvalidate(model, lm, emissions, cfd)
 
 if __name__ == "__main__": main()
