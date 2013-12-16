cdb893d611d2de99fa13c4d41b8d6e62f3380f36
Alex Rudnick <alex.rudnick@gmail.com>
2013-12-16 14:42:19 -0500

    check in mfs

--- hmmwsd/searches.py
+++ hmmwsd/searches.py
@@ -9,6 +9,7 @@ from operator import itemgetter
 from nltk.probability import DictionaryProbDist
 
 from constants import OOV
+from constants import UNTRANSLATED
 from util_search import build_vocab
 from util_search import transition_logprob
 import memm_features
@@ -118,6 +119,29 @@ def backoff_conditional_distribution(tagged_sent, t, vocab, hmmparts):
         emission_penalty = -hmmparts.emissions[newword].logprob(sw)
         newpenalty = transition_penalty + emission_penalty
         out.append((newword, newpenalty))
+    return out
+
+def maxent_mfs(unlabeled_sequence, cfd):
+    """If we have a classifier handy, use that. Otherwise, take the MFS."""
+    out = []
+    classifiers = list(map(picklestore.get, unlabeled_sequence)) ## AWWW YEAH.
+    T = len(unlabeled_sequence)
+    for t in range(0, T):
+        symbol = unlabeled_sequence[t]
+        if classifiers[t]:
+            features = memm_features.extract(out, t)
+            besttag = classifiers[t].classify(features)
+            # print("predicted with maxent:", (symbol, besttag))
+            out.append((symbol, besttag))
+        else:
+            bestscore = float('-inf')
+            besttag = UNTRANSLATED
+            for state in cfd[symbol].samples():
+                score = cfd['symbol'][state]
+                if score > bestscore:
+                    bestscore = score
+                    besttag = state
+            out.append((symbol, besttag))
     return out
 
 def beam_memm(unlabeled_sequence, hmmparts, beamwidth=5):
