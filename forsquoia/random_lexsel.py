f2f8f73d49855e966e2e308231908663011988e3
Alex Rudnick <alex.rudnick@gmail.com>
2014-01-16 01:24:57 -0500

    starting to pull out useful bits from the incoming xml

--- forsquoia/random_lexsel.py
+++ forsquoia/random_lexsel.py
@@ -4,6 +4,8 @@ import random
 import sys
 import xml.etree.ElementTree as ET
 
+from util import dprint
+
 def make_decision(node):
     """Make a potentially-terrible decision."""
     options = [child for child in node if child.tag == 'SYN']
@@ -16,11 +18,38 @@ def make_decision(node):
     for option in options:
         node.remove(option)
 
+def prettify(elem):
+    from xml.dom import minidom
+    """Return a pretty-printed XML string for the Element."""
+    rough_string = ET.tostring(elem, 'unicode')
+    ## print(type(rough_string), file=sys.stderr)
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+def get_tokens(corpus):
+    """Find all the nodes in the tree, return the list of source-language
+    tokens."""
+    target_nodes = corpus.findall(".//NODE")
+    tokens = []
+    for node in target_nodes:
+        ref = node.attrib['ref']
+        sform = node.attrib['sform']
+        slem = node.attrib['slem']
+        tokens.append((ref,sform, slem))
+    tokens.sort()
+    dprint(tokens)
+
 def main():
     lines = []
     for line in sys.stdin:
-        lines.append(line)
+        if line.strip():
+            lines.append(line.strip())
     corpus = ET.fromstringlist(lines)
+
+    dprint("!" * 80)
+    get_tokens(corpus)
+    dprint(prettify(corpus))
+    dprint("!" * 80)
 
     ## find all the NODE elements in the tree that have a SYN underneath them
     target_nodes = corpus.findall(".//NODE/SYN/..")
--- forsquoia/util.py
+++ forsquoia/util.py
@@ -1,5 +1,10 @@
+import sys
+
 def print_lemmatized_sentences(sentences):
     for sent in sentences:
         line = " ".join(lemma for (token,lemma) in sent)
         if line.startswith("-- warning"): continue
         print(line)
+
+def dprint(*a,**aa):
+    print(file=sys.stderr, *a, **aa)
