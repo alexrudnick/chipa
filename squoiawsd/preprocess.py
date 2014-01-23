53ba6f3eeb6bf659d663a43cb4b669a77fb09717
Alex Rudnick <alex.rudnick@gmail.com>
2014-01-23 17:42:22 -0500

    working on Spanish preprocessing and running on Quechua test set

---
+++ squoiawsd/preprocess.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+import freeling
+import sys
+
+## Modify this line to be your FreeLing installation directory
+FREELINGDIR = "/usr/local";
+
+DATA = FREELINGDIR+"/share/freeling/";
+LANG="es";
+
+freeling.util_init_locale("default");
+
+# create options set for maco analyzer. Default values are Ok, except for data files.
+op= freeling.maco_options("es");
+op.set_active_modules(0,1,1,1,1,1,1,1,1,1);
+op.set_data_files("",DATA+LANG+"/locucions.dat", DATA+LANG+"/quantities.dat", 
+                  DATA+LANG+"/afixos.dat", DATA+LANG+"/probabilitats.dat", 
+                  DATA+LANG+"/dicc.src", DATA+LANG+"/np.dat",  
+                  DATA+"common/punct.dat");
+
+# create analyzers
+tk=freeling.tokenizer(DATA+LANG+"/tokenizer.dat");
+sp=freeling.splitter(DATA+LANG+"/splitter.dat");
+mf=freeling.maco(op);
+
+tg=freeling.hmm_tagger(DATA+LANG+"/tagger.dat",1,2);
+#sen=freeling.senses(DATA+LANG+"/senses.dat");
+#parser= freeling.chart_parser(DATA+LANG+"/chunker/grammar-chunk.dat");
+#dep=freeling.dep_txala(DATA+LANG+"/dep/dependences.dat", parser.get_start_symbol());
+
+import fileinput
+for line in fileinput.input():
+    l = tk.tokenize(line)
+    ls = sp.split(l,0)
+
+    ls = mf.analyze(ls)
+    ls = tg.analyze(ls)
+    for s in ls:
+        ws = s.get_words()
+        for w in ws:
+            print("{0} {1} {2}".format(
+                w.get_form(), w.get_lemma(), w.get_tag()))
+        print()
--- squoiawsd/qu_experiment.py
+++ squoiawsd/qu_experiment.py
@@ -9,29 +9,39 @@ import random
 import nltk
 
 import searches
-import learn
+import preprocess
 
-def randomvalidate(model, cfd):
+TESTSET="testdata/defensoria1-3_es.txt"
+
+def test_on_testset(cfd):
     totalcorrect = 0
     totalwords = 0
-    for lineid in guarani.testset:
-        tagged = searches.maxent_mfs(ss, cfd)
-        print("ORIGINAL:", list(zip(ss,ts)))
-        print("TAGGED:", tagged)
-        predicted = [t for (s,t) in tagged]
-        correct = 0
-        print(list(zip(ts, predicted)))
-        for actual, pred in zip(ts, predicted):
-            if actual == UNTRANSLATED: continue
-            totalwords += 1
-            if actual == pred:
-                correct += 1
-                totalcorrect += 1
-        print("sentence accuracy:", correct / len(ss))
-        print("considered words:", len(ss))
-    accuracy = (totalcorrect / totalwords)
-    print("accuracy:", accuracy)
-    print("considered words:", totalwords)
+    with open(TESTSET) as infile:
+        for line in infile:
+            ## preprocess that line
+
+            ## run classifiers over all the words...
+
+            ## hrm, how do we handle alignments in this case?
+
+            print(line)
+            #tagged = searches.maxent_mfs(ss, cfd)
+            #print("ORIGINAL:", list(zip(ss,ts)))
+            #print("TAGGED:", tagged)
+            #predicted = [t for (s,t) in tagged]
+            #correct = 0
+            #print(list(zip(ts, predicted)))
+            #for actual, pred in zip(ts, predicted):
+            #    if actual == UNTRANSLATED: continue
+            #    totalwords += 1
+            #    if actual == pred:
+            #        correct += 1
+            #        totalcorrect += 1
+            #print("sentence accuracy:", correct / len(ss))
+            #print("considered words:", len(ss))
+    #accuracy = (totalcorrect / totalwords)
+    #print("accuracy:", accuracy)
+    #print("considered words:", totalwords)
 
 def get_argparser():
     parser = argparse.ArgumentParser(description='quechua')
@@ -44,9 +54,7 @@ def main():
     parser = get_argparser()
     args = parser.parse_args()
     assert args.model in ["uniform", "mfs", "maxent"]
-
     model = args.model
-
     randomvalidate(model, cfd)
 
 if __name__ == "__main__": main()
