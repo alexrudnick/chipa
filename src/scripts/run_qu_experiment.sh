3c7126d0b416a2044bafd93dbdeaffdf1f96e295
Alex Rudnick <alex.rudnick@gmail.com>
2014-12-20 21:50:35 -0800

    remove old script

--- src/scripts/run_qu_experiment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 qu_experiment.py \
-  --sourcefn /space/output_es_qu/training.es.txt \
-  --targetfn /space/output_es_qu/training.qu.txt \
-  --alignfn /space/output_es_qu/training.align \
-  --crossvalidate \
-  "$@" \
