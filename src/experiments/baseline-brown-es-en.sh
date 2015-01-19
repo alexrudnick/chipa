d6b92a11802e7de34dc72fd7a28fbd927c696329
Alex Rudnick <alex.rudnick@gmail.com>
2015-01-18 22:39:29 -0800

    move experiment-running scripts into their own directory

---
+++ src/experiments/baseline-brown-es-en.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## es-en with brown features too
+python3 clwsd_experiment.py \
+  --bitext ~/terere/bibletools/output/bible.es-en \
+  --alignfn ~/terere/bibletools/output/bible.es-en.align \
+  --featurefn featuresets/default_with_brown.features  \
+  --annotatedfn ~/terere/bibletools/output/bible.es-en.source.annotated
---
+++ src/experiments/baseline-brown-es-gn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+## es-gn with brown features too
+python3 clwsd_experiment.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --featurefn featuresets/default_with_brown.features  \
+  --annotatedfn ~/terere/bibletools/output/bible.es-gn.source.annotated
