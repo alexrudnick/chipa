b73a16b1fffa50913a4efb1c056aea0bea67b83a
Alex Rudnick <alex.rudnick@gmail.com>
2015-01-26 00:13:49 -0800

    script to take an annotated corpus and a Brown clusters 'path' file and add the Brown clusters for those words.

---
+++ src/annotate_brown.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""
+Go over an annotated corpus file and add Brown cluster annotations for the words
+in it.
+"""
+import argparse
+
+import annotated_corpus
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='annotate_brown')
+    parser.add_argument('--annotatedfn', type=str, required=True)
+    parser.add_argument('--clusterfn', type=str, required=True)
+    parser.add_argument('--featureprefix', type=str, required=True)
+    parser.add_argument('--lemmas', type=bool, required=False)
+    return parser
+
+def load_word_to_cluster(clusterfn):
+    out = {}
+    with open(clusterfn) as infile:
+        for line in infile:
+            cluster, word, count = line.strip().split('\t')
+            out[word] = cluster
+    return out
+
+def main():
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    word_to_cluster = load_word_to_cluster(args.clusterfn)
+    corpus = annotated_corpus.load_corpus(args.annotatedfn)
+
+    for sentence in corpus:
+        for token in sentence:
+            w = token.lemma if args.lemmas else token.surface
+            if w in word_to_cluster:
+                token.annotations.add(args.featureprefix +
+                                      "=" +
+                                      word_to_cluster[w])
+            print(token)
+        print()
+
+if __name__ == "__main__": main()
