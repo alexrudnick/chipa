fb721a2a7093370e45d8a0bca0bae45ff27297fc
Alex Rudnick <alex.rudnick@gmail.com>
2016-01-04 17:40:51 -0800

    update script to score both test sets

--- src/scripts/score_semeval.sh
+++ src/scripts/score_semeval.sh
@@ -5,20 +5,42 @@ WORDS="coach education execution figure job letter match mission mood paper post
 
 for THELANG in $THELANGS; do
   for WORD in $WORDS; do
-    echo perl scripts/ScorerTask3.pl ../semevaloutput/"$WORD"."$THELANG".oof ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt -t oof
-    perl scripts/ScorerTask3.pl ../semevaloutput/"$WORD"."$THELANG".oof ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt -t oof
+    echo perl scripts/ScorerTask3.pl ../semeval_2013_output/"$WORD"."$THELANG".oof ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt -t oof
+    perl scripts/ScorerTask3.pl ../semeval_2013_output/"$WORD"."$THELANG".oof ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt -t oof
   done
 done
 
 for THELANG in $THELANGS; do
   for WORD in $WORDS; do
-    echo perl scripts/ScorerTask3.pl ../semevaloutput/"$WORD"."$THELANG".best ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt
-    perl scripts/ScorerTask3.pl ../semevaloutput/"$WORD"."$THELANG".best ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt
+    echo perl scripts/ScorerTask3.pl ../semeval_2013_output/"$WORD"."$THELANG".best ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt
+    perl scripts/ScorerTask3.pl ../semeval_2013_output/"$WORD"."$THELANG".best ../semeval_2013_gold/"$THELANG"/"$WORD"_gold.txt
+  done
+done
+
+
+for THELANG in $THELANGS; do
+  for WORD in $WORDS; do
+    echo perl scripts/ScorerTask3.pl ../semeval_2010_output/"$WORD"."$THELANG".oof ../semeval_2010_gold/"$WORD".gold."$THELANG" -t oof
+    perl scripts/ScorerTask3.pl ../semeval_2010_output/"$WORD"."$THELANG".oof ../semeval_2010_gold/"$WORD".gold."$THELANG" -t oof
+  done
+done
+
+for THELANG in $THELANGS; do
+  for WORD in $WORDS; do
+    echo perl scripts/ScorerTask3.pl ../semeval_2010_output/"$WORD"."$THELANG".best ../semeval_2010_gold/"$WORD".gold."$THELANG"
+    perl scripts/ScorerTask3.pl ../semeval_2010_output/"$WORD"."$THELANG".best ../semeval_2010_gold/"$WORD".gold."$THELANG"
   done
 done
 
 echo "==== 2013 precision best ===="
-cat ../semevaloutput/*.best.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
+cat ../semeval_2013_output/*.best.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
 
 echo "==== 2013 precision oof ===="
-cat ../semevaloutput/*.oof.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
+cat ../semeval_2013_output/*.oof.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
+
+
+echo "==== 2010 precision best ===="
+cat ../semeval_2010_output/*.best.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
+
+echo "==== 2010 precision oof ===="
+cat ../semeval_2010_output/*.oof.results | grep "^precision =" | cut -f 3 -d " " | sed "s/,//g"  | sum_numbers.py
