721152d12ffded1a070ccd3e3b56279294eb51b8
Alex Rudnick <alex.rudnick@gmail.com>
2016-01-10 22:05:24 -0800

    remove obsolete script

--- src/scripts/runlearn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 learn.py \
-  --sourcefn /space/output_es_qu/training.es.txt \
-  --targetfn /space/output_es_qu/training.qu.txt \
-  --alignfn /space/output_es_qu/training.align \
-  --clusterfn ./europarl-c1000.paths \
