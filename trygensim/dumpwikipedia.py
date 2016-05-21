f41090208dda532354d767c01bb85bfe2b777834
Alex Rudnick <alex.rudnick@gmail.com>
2016-05-20 22:30:05 -0700

    use nltk sentence splitting and tokenization so we don't drop non-alphabetic tokens

--- trygensim/dumpwikipedia.py
+++ trygensim/dumpwikipedia.py
@@ -4,18 +4,23 @@
 """
 
 import sys
+
 import nltk
+import gensim
 from gensim.corpora import WikiCorpus
 
 import os.path
 import sys
 
-def main():
-    infn, outfn = sys.argv[1:3]
-    i = 0
+## We're going to replace gensim.corpora.wikicorpus.tokenize with this, so that
+## we get NLTK sentence splitting and don't drop punctuation.
+def replacement_tokenize(content):
+    return [token.encode('utf-8') for token in nltk.word_tokenize(content)]
 
-    ## XXX: this seems to be dropping punctuation currently, so we're not
-    ## getting sentence splitting.
+def main():
+    gensim.corpora.wikicorpus.tokenize = replacement_tokenize
+
+    infn, outfn = sys.argv[1:3]
     wiki = WikiCorpus(infn, lemmatize=False, dictionary={})
     with open(outfn, 'w') as outfile:
         for i, article in enumerate(wiki.get_texts()):
