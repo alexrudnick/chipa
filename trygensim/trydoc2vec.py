65304a064f9333629cd54cd32c81f993dd1f8a17
Alex Rudnick <alex.rudnick@gmail.com>
2016-05-23 23:35:22 -0700

    tiny doc2vec model for experimentation

--- trygensim/trydoc2vec.py
+++ trygensim/trydoc2vec.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+import argparse
 import multiprocessing
 
 from gensim.models import Doc2Vec
@@ -6,6 +9,14 @@ from collections import OrderedDict
 
 CORPUS = "/space/spanish-wikipedia/spanish-wikipedia.txt"
 SAVED_MODEL = "spanish-wikipedia-doc2vec.model"
+
+TINY_SAVED_MODEL = "europarl-es-10k-doc2vec.model"
+TINYCORPUS = "europarl-es-10k.txt"
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='trydoc2vec')
+    parser.add_argument('--tiny', type=bool, default=False, required=False)
+    return parser
 
 def magnitude(vec):
     import math
@@ -22,7 +33,16 @@ def main():
     cores = multiprocessing.cpu_count()
     assert gensim.models.doc2vec.FAST_VERSION > -1, "need fast version"
 
-    documents = gensim.models.doc2vec.TaggedLineDocument(CORPUS)
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    thecorpus = CORPUS
+    savedmodel = SAVED_MODEL
+    if args.tiny:
+        thecorpus = TINYCORPUS
+        savedmodel = TINY_SAVED_MODEL
+
+    documents = gensim.models.doc2vec.TaggedLineDocument(thecorpus)
     ## XXX: tunable parameter. 100 seems slightly small.
     model = Doc2Vec(documents, size=100, window=8, min_count=1, workers=cores)
 
