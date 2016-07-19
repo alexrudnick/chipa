3994a2fdaf24421b7bff4517a4f3a3b0e08c94a4
Alex Rudnick <alex.rudnick@gmail.com>
2016-07-18 22:51:15 -0700

    add gensim to main source tree

--- src/scripts/setup_virtualenv.sh
+++ src/scripts/setup_virtualenv.sh
@@ -9,5 +9,6 @@ pip install scikit-learn
 pip install pyyaml
 pip install nose
 pip install https://github.com/nltk/nltk/archive/3.0.4.zip
+pip install --upgrade gensim
 ## make sure we have sentence segmenter models
 ## python3 -m nltk.downloader punkt
