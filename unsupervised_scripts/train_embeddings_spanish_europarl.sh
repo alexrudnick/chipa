bc5ba41845b3a2b2a48e4cae58d9dbca7e1555c3
Alex Rudnick <alex.rudnick@gmail.com>
2016-11-06 01:18:22 -0700

    changing file paths around

--- unsupervised_scripts/train_embeddings_spanish_europarl.sh
+++ unsupervised_scripts/train_embeddings_spanish_europarl.sh
@@ -3,16 +3,16 @@
 WORD2VECDIR=/home/alex/checkouts/word2vec/
 WORD2VEC=$WORD2VECDIR/word2vec
 WORD2PHRASE=$WORD2VECDIR/word2phrase
-CORPUS=/tmp/europarl-v7.es-en.es.tok
+CORPUS=/space/build_word2vec/europarl-v7.es-en.es.tok
 
-PHRASECORPUS=/tmp/europarl-v7.phrase
-PHRASECORPUS2=/tmp/europarl-v7.phrase2
+PHRASECORPUS=/space/build_word2vec/europarl-v7.phrase
+PHRASECORPUS2=/space/build_word2vec/europarl-v7.phrase2
 
-if [ ! -e "/tmp/europarl-v7.es-en.es.tok" ]; then
+if [ ! -e "$CORPUS" ]; then
   echo "tokenizing..."
   ~/cdec/corpus/tokenize-anything.sh \
     < /space/europarl-parallel/europarl-v7.es-en.es \
-    > /tmp/europarl-v7.es-en.es.tok
+    > $CORPUS
 fi
 
 time $WORD2PHRASE -train $CORPUS -output $PHRASECORPUS -threshold 100 -debug 2
