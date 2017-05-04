9363ecee9e01157a1465d3cdaf9680dc2c747a5c
Alex Rudnick <alex.rudnick@gmail.com>
2017-05-03 23:51:20 -0700

    train word2vec with higher min-count flags

--- unsupervised_scripts/train_embeddings_spanish_europarl.sh
+++ unsupervised_scripts/train_embeddings_spanish_europarl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORD2VECDIR=/home/alex/checkouts/word2vec/
+WORD2VECDIR=$HOME/checkouts/word2vec/
 WORD2VEC=$WORD2VECDIR/word2vec
 WORD2PHRASE=$WORD2VECDIR/word2phrase
 CORPUS=/space/build_word2vec/europarl-v7.es-en.es.tok
@@ -21,39 +21,39 @@ time $WORD2PHRASE -train $PHRASECORPUS -output $PHRASECORPUS2 -threshold 100 -de
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-400.cbow \
   -cbow 1 -size 400 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-400.skipgram \
   -cbow 0 -size 400 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-200.cbow \
   -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-200.skipgram \
   -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-100.cbow \
   -cbow 1 -size 100 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-100.skipgram \
   -cbow 0 -size 100 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-50.cbow \
   -cbow 1 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-europarl-50.skipgram \
   -cbow 0 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 20
--- unsupervised_scripts/train_embeddings_spanish_wikipedia.sh
+++ unsupervised_scripts/train_embeddings_spanish_wikipedia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WORD2VECDIR=/home/alex/checkouts/word2vec/
+WORD2VECDIR=$HOME/checkouts/word2vec/
 WORD2VEC=$WORD2VECDIR/word2vec
 WORD2PHRASE=$WORD2VECDIR/word2phrase
 CORPUS=/space/spanish-wikipedia/spanish-wikipedia.txt
@@ -14,39 +14,39 @@ time $WORD2PHRASE -train $PHRASECORPUS -output $PHRASECORPUS2 -threshold 100 -de
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-400.cbow \
   -cbow 1 -size 400 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-400.skipgram \
   -cbow 0 -size 400 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-200.cbow \
   -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-200.skipgram \
   -cbow 0 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-100.cbow \
   -cbow 1 -size 100 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-100.skipgram \
   -cbow 0 -size 100 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-50.cbow \
   -cbow 1 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
 
 time $WORD2VEC -train $PHRASECORPUS2 \
   -output /space/clustering/word2vec-spanish-wikipedia-50.skipgram \
   -cbow 0 -size 50 -window 8 -negative 25 -hs 0 -sample 1e-4 \
-  -threads 20 -binary 0 -iter 15
+  -threads 20 -binary 0 -iter 15 -min-count 100
