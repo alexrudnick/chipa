7008e883ae4b11824fe0af492e6bc74412ef05e4
Alex Rudnick <alex.rudnick@gmail.com>
2017-05-14 23:52:35 -0700

    script for running experiment with all the different embeddings

---
+++ src/experiments/experiment_es-gn-every-embedding.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-europarl-50.cbow \
+  --embedding_dim 50
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-europarl-100.cbow \
+  --embedding_dim 100
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-europarl-200.cbow \
+  --embedding_dim 200
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-europarl-400.cbow \
+  --embedding_dim 400
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-wikipedia-50.cbow \
+  --embedding_dim 50
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-wikipedia-100.cbow \
+  --embedding_dim 100
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-wikipedia-200.cbow \
+  --embedding_dim 200
+
+python3 clwsd_experiment_embeddings.py \
+  --bitext ~/terere/bibletools/output/bible.es-gn \
+  --alignfn ~/terere/bibletools/output/bible.es-gn.align \
+  --annotatedfn ~/chipa/src/annotated/bible.es-gn.source.annotated \
+  --embeddings /space/clustering/word2vec-spanish-wikipedia-400.cbow \
+  --embedding_dim 400
