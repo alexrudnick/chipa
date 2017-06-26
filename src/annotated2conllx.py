52631b20767dfe6f0f1a02f5c8799898e29cedb5
Alex Rudnick <alex.rudnick@gmail.com>
2017-06-25 23:59:30 -0700

    add annotated2conllx.py

---
+++ src/annotated2conllx.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+Go over an annotated corpus file and turn it into CONLL-X format, so we can run
+MaltParser over it.
+"""
+import argparse
+
+import annotated_corpus
+
+def get_argparser():
+    parser = argparse.ArgumentParser(description='annotated2connlx')
+    parser.add_argument('--annotatedfn', type=str, required=True)
+    return parser
+
+def main():
+    parser = get_argparser()
+    args = parser.parse_args()
+
+    corpus = annotated_corpus.load_corpus(args.annotatedfn)
+
+    for sentence in corpus:
+        for index, token in enumerate(sentence):
+            position = index + 1
+            print("{0}\t{1}\t{2}\t{3}\t-\t{4}\t-\t-\t-".format(
+                  position, token.surface, token.lemma,
+                  token.annotations["tag"]))
+                  ## TODO: use universal pos tags to get coarse tag?
+        print()
+
+if __name__ == "__main__": main()
