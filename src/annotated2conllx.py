980d2a7ed40e75f2dfccfce096b22cd6e68566ce
Alex Rudnick <alex.rudnick@gmail.com>
2017-06-27 23:59:20 -0700

    coarse tags

--- src/annotated2conllx.py
+++ src/annotated2conllx.py
@@ -13,6 +13,42 @@ def get_argparser():
     parser.add_argument('--annotatedfn', type=str, required=True)
     return parser
 
+def lookup_coarse_tag(tag):
+    ## tags are: ADJ ADV INTJ NOUN PROPN VERB
+    if tag.startswith("A"):
+        return "ADJ"
+    if tag.startswith("R"):
+        return "ADV"
+    if tag.startswith("NC"):
+        return "NOUN"
+    if tag.startswith("NP"):
+        return "PROPN"
+    if tag.startswith("V"):
+        return "VERB"
+    if tag.startswith("I"):
+        return "INTJ"
+
+    ##           ADP AUX CCONJ DET NUM PART PRON SCONJ
+    if tag.startswith("S"):
+        return "ADP"
+    if tag.startswith("P"):
+        return "PRON"
+    if tag.startswith("CC"):
+        return "CCONJ"
+    if tag.startswith("CS"):
+        return "SCONJ"
+    if tag.startswith("D"):
+        return "DET"
+    if tag.startswith("Z"):
+        return "NUM"
+    ##           PUNCT SYM X
+    if tag.startswith("F"):
+        return "PUNCT"
+
+    ## TODO: be able to detect X or SYM?
+
+    return "-"
+
 def main():
     parser = get_argparser()
     args = parser.parse_args()
@@ -29,9 +65,11 @@ def main():
                 if k == "tag":
                     tag = v
 
+            coarsetag = lookup_coarse_tag(tag)
+
             # id form lemma cpostag postag feats head deprel phead pdeprel
             print("{0}\t{1}\t{2}\t{3}\t{4}\t-\t-\t-\t-\t-".format(
-                position, token.surface, token.lemma, "-", tag))
+                position, token.surface, token.lemma, coarsetag, tag))
             ## TODO: use universal pos tags to get coarse tag?
         print()
 
