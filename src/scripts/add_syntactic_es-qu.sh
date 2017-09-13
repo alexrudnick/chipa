d7378240a911f36c5071502e4bd9aff329ab98b3
Alex Rudnick <alex.rudnick@gmail.com>
2017-09-13 00:20:41 -0700

    add maltparser command to the script to add syntactic annotations

--- src/scripts/add_syntactic_es-qu.sh
+++ src/scripts/add_syntactic_es-qu.sh
@@ -2,6 +2,15 @@
 
 cp ~/terere/bibletools/output/bible.es-qu.source.annotated annotated/tmp.annotated
 
+python3 annotated2conllx.py --annotatedfn annotated/tmp.annotated \
+  > ~/chipa/src/annotated/bible.es-qu.source.conll
+
+pushd ~/chipa/maltparser/espmalt
+java -Xmx4096m -jar ~/chipa/maltparser/maltparser-1.9.0/maltparser-1.9.0.jar \
+  -c espmalt-1.0 -i ~/chipa/src/annotated/bible.es-qu.source.conll \
+  -o ~/chipa/src/annotated/bible.es-qu.source.conll.parsed -m parse
+popd
+
 python3 ./annotate_syntactic.py \
   --annotatedfn annotated/tmp.annotated \
   --conllfn ~/chipa/src/annotated/bible.es-qu.source.conll.parsed \
