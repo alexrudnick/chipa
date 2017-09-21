31d8f7b23a53f8ef5b0fae51da4883a9c8a15b53
Alex Rudnick <alex.rudnick@gmail.com>
2017-09-20 23:57:55 -0700

    update script to include clusters from lemmatized wikipedia

--- src/scripts/add_brown_clusters_es-gn.sh
+++ src/scripts/add_brown_clusters_es-gn.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
-cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/tmp.annotated
+cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/input.annotated
 
 python3 annotate_brown.py \
-  --annotatedfn annotated/tmp.annotated \
+  --annotatedfn annotated/input.annotated \
   --clusterfn /space/clustering/brown-spanish-europarl/paths \
   --featureprefix brown_europarl \
-  > annotated/tmp2.annotated
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
 
 python3 annotate_brown.py \
-  --annotatedfn annotated/tmp2.annotated \
+  --annotatedfn annotated/input.annotated \
   --clusterfn /space/clustering/brown-spanish-europarl-lemmas/paths \
   --featureprefix brown_europarl_lemma \
   --lemmas True \
-  > annotated/tmp3.annotated
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
 
 python3 annotate_brown.py \
-  --annotatedfn annotated/tmp3.annotated \
+  --annotatedfn annotated/input.annotated \
   --clusterfn /space/clustering/brown-spanish-wikipedia/paths \
   --featureprefix brown_wikipedia \
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
+
+python3 annotate_brown.py \
+  --annotatedfn annotated/input.annotated \
+  --clusterfn /space/clustering/brown-spanish-wikipedia-lemmas/paths \
+  --featureprefix brown_wikipedia_lemma \
+  --lemmas True \
   > annotated/bible.es-gn.source.annotated
 
-rm -f annotated/tmp.annotated annotated/tmp2.annotated annotated/tmp3.annotated
+rm -f annotated/input.annotated
+rm -f annotated/output.annotated
