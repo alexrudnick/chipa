b4e7622f4a8f7d3403f83019df471ee4b6468ab1
Alex Rudnick <alex.rudnick@gmail.com>
2017-09-24 21:18:18 -0700

    add featureset, scripts and experiments for using both brown clusters and syntactic features at the same time

---
+++ src/scripts/add_brown_syntactic_es-gn.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/input.annotated
+
+python3 annotate_brown.py \
+  --annotatedfn annotated/input.annotated \
+  --clusterfn /space/clustering/brown-spanish-europarl/paths \
+  --featureprefix brown_europarl \
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
+
+python3 annotate_brown.py \
+  --annotatedfn annotated/input.annotated \
+  --clusterfn /space/clustering/brown-spanish-europarl-lemmas/paths \
+  --featureprefix brown_europarl_lemma \
+  --lemmas True \
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
+
+python3 annotate_brown.py \
+  --annotatedfn annotated/input.annotated \
+  --clusterfn /space/clustering/brown-spanish-wikipedia/paths \
+  --featureprefix brown_wikipedia \
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
+
+python3 annotate_brown.py \
+  --annotatedfn annotated/input.annotated \
+  --clusterfn /space/clustering/brown-spanish-wikipedia-lemmas/paths \
+  --featureprefix brown_wikipedia_lemma \
+  --lemmas True \
+  > annotated/output.annotated
+
+mv annotated/output.annotated annotated/input.annotated
+
+python3 ./annotate_syntactic.py \
+  --annotatedfn annotated/input.annotated \
+  --conllfn ~/chipa/src/annotated/bible.es-gn.source.conll.parsed \
+  > annotated/output.annotated
+
+rm -f annotated/input.annotated
+mv annotated/output.annotated annotated/bible.es-gn.source.annotated
