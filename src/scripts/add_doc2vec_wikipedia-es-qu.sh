443557d4c97653410f09ebb1554434f48e53218c
Alex Rudnick <alex.rudnick@gmail.com>
2017-10-30 23:49:32 -0700

    update scripts for annotating with doc2vec

--- src/scripts/add_doc2vec_wikipedia-es-qu.sh
+++ src/scripts/add_doc2vec_wikipedia-es-qu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/tmp.annotated
+cp ~/terere/bibletools/output/bible.es-qu.source.annotated annotated/tmp.annotated
 
 python3 annotate_doc2vec.py \
   --annotatedfn annotated/tmp.annotated \
   --doc2vecmodel /space/chipa/trygensim/200-spanish-wikipedia-doc2vec.model \
   --featureprefix doc2vec_wikipedia \
-  > annotated/bible.es-gn.source.annotated
+  > annotated/bible.es-qu.source.annotated
 
 rm -f annotated/tmp.annotated
