1ec5277b6f9a456d5d946fa54511848b3aa0b483
Alex Rudnick <alex.rudnick@gmail.com>
2017-11-24 21:57:44 -0800

    collate results into the format we're using for the latex, to make it easier to build tables

--- src/visualizeresults.py
+++ src/visualizeresults.py
@@ -18,10 +18,27 @@ def langpair(fn):
         print("filename:", fn)
         sys.exit(1)
 
+def name_to_setting(name, lp):
+    before, after = name.split("-" + lp + "-")
+    assert (after.endswith("-regular") or after.endswith("-nonnull"))
+
+    if after.endswith("-regular"):
+        return after.split("-regular")[0]
+    if after.endswith("-nonnull"):
+        return after.split("-nonnull")[0]
+    assert False, "this can't happen"
+
+
 def main():
     # (accuracy, name, word_to_total, word_to_correct)
     file_overviews_regular = []
     file_overviews_nonnull = []
+
+    # Gonna map a classifier setting to a list of 4 numbers.
+    # A classifier setting is everything after the language pair in the name but
+    # before "-regular" or "-nonnull"
+    settings_to_scores = defaultdict(lambda: [0, 0, 0, 0])
+
     for fn in sys.argv[1:]:
         name = os.path.basename(fn)
         lp = langpair(fn)
@@ -46,12 +63,33 @@ def main():
         overall_accuracy = None
         if overall_words:
             overall_accuracy = overall_correct / overall_words 
+
+            setting = name_to_setting(name, lp)
+
+            if lp == "es-gn" and "-regular" in name:
+                settings_to_scores[setting][0] = overall_accuracy
+            if lp == "es-gn" and "-nonnull" in name:
+                settings_to_scores[setting][1] = overall_accuracy
+            if lp == "es-qu" and "-regular" in name:
+                settings_to_scores[setting][2] = overall_accuracy
+            if lp == "es-qu" and "-nonnull" in name:
+                settings_to_scores[setting][3] = overall_accuracy
+
             if "regular" in name:
                 file_overviews_regular.append((overall_accuracy, name,
                                                word_to_correct, word_to_total))
             elif "nonnull" in name:
                 file_overviews_nonnull.append((overall_accuracy, name,
                                                word_to_correct, word_to_total))
+
+    for setting in sorted(settings_to_scores.keys()):
+        scores = settings_to_scores[setting]
+        print("{0} & {1:.3f} & {2:.3f} & {3:.3f} & {4:.3f}".format(setting,
+              scores[0], scores[1], scores[2], scores[3]))
+
+    print()
+    print()
+    print("#" * 80)
 
     for (acc, name, w2c, w2t) in sorted(file_overviews_regular, reverse=True):
         print("{0}\t{1:.3f}".format(name, acc))
