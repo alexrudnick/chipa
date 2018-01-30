74401e94cbb83c2715e142b86ceb32b80b3484f6
Alex Rudnick <alex.rudnick@gmail.com>
2018-01-29 23:11:16 -0800

    now we only load training data for one lemma at a time, similar to how we did it for semeval experiments

--- src/annotate_clwsd.py
+++ src/annotate_clwsd.py
@@ -6,6 +6,7 @@ that we can.
 """
 
 from collections import defaultdict
+from collections import namedtuple
 from operator import itemgetter
 import argparse
 import os
@@ -36,29 +37,41 @@ def get_argparser():
     parser.add_argument('--overwrite', type=bool, default=False, required=True)
     return parser
 
+Filenames = namedtuple("Filenames", ["bitextfn", "alignfn", "annotatedfn"])
+
 ## OK, how are we going to do this?
 ## We need to take a complete Chipa configuration and use this to train
 ## classifiers on demand. Then we blow through the input annotated corpus and we
 ## output classifications for that.
 
-def setup_training_data(args):
-    ## XXX: maybe move this to trainingdata.py
-    triple_sentences = trainingdata.load_bitext(args.bitextfn, args.alignfn)
+#### stuff for loading training data from disk on demand
+
+## XXX: this is copypasta from semeval_experiment.py
+def load_training_for_word(word, bitextfn, alignfn, annotatedfn):
+    triple_sentences = trainingdata.load_bitext_for_word(word, bitextfn, alignfn)
     tl_sentences = trainingdata.get_target_language_sentences(triple_sentences)
     sl_sentences = [s for (s,t,a) in triple_sentences]
     tagged_sentences = [list(zip(ss, ts))
                         for ss,ts in zip(sl_sentences, tl_sentences)]
     trainingdata.set_examples(sl_sentences,tagged_sentences)
 
-    source_annotated = annotated_corpus.load_corpus(args.annotatedfn)
+    source_annotated = annotated_corpus.load_corpus_for_word(word, annotatedfn)
     trainingdata.set_sl_annotated(source_annotated)
+    print("TRAINING DATA LOADED.")
+
+#### end stuff for loading training data from disk on demand
 
 @functools.lru_cache(maxsize=100000)
-def classifier_for_lemma(lemma):
+def classifier_for_lemma(lemma, filenames):
     # XXX: always doing nullable and Random Forest for initial version
     classifier = SklearnClassifier(RandomForestClassifier(), sparse=False)
     print("loading training data for", lemma)
-    training = trainingdata.trainingdata_for(lemma, nonnull=False)
+    load_training_for_word(lemma, filenames.bitextfn, filenames.alignfn,
+                           filenames.annotatedfn)
+
+    training = trainingdata.trainingdata_for(lemma, nonnull=True)
+    print("got {0} instances for {1}".format(len(training), lemma))
+
     labels = set(label for (feat,label) in training)
     print("loaded training data for", lemma)
     if (not training) or len(labels) < 2:
@@ -66,12 +79,12 @@ def classifier_for_lemma(lemma):
     classifier.train(training)
     return classifier
 
-def predict_class(sentence, index):
+def predict_class(sentence, index, filenames):
     """Predict a translation for the token at the current index in this
     annotated sentence."""
 
     lemma = sentence[index].lemma
-    classifier = classifier_for_lemma(lemma)
+    classifier = classifier_for_lemma(lemma, filenames)
     if not classifier:
         return None
 
@@ -95,18 +108,19 @@ def main():
     features.load_featurefile(args.featurefn)
     trainingdata.STOPWORDS = trainingdata.load_stopwords(args.bitextfn)
 
+    filenames = Filenames(bitextfn=args.bitextfn,
+                          alignfn=args.alignfn,
+                          annotatedfn=args.annotatedfn)
+
     language_pair = args.annotated_to_classify.split(".")[1]
     top_words = set(list_focus_words.load_top_words(language_pair))
-
-    setup_training_data(args)
-    print("training data has been loaded")
 
     corpus = annotated_corpus.load_corpus(args.annotated_to_classify)
     for sentence in corpus:
         for i, token in enumerate(sentence):
             if token.lemma not in top_words: continue
 
-            predicted = predict_class(sentence, i)
+            predicted = predict_class(sentence, i, filenames)
             if predicted:
                 token.annotations.add(args.featureprefix + "=" + predicted)
             if not args.overwrite:
