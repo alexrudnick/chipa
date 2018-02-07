05c2e593cf4e575b4fa476de893f34bf0b67ba5b
Alex Rudnick <alex.rudnick@gmail.com>
2018-02-06 23:57:27 -0800

    don't clear out annotations before we add stacking annotations

--- src/scripts/fr_add_stacking_es-gn.sh
+++ src/scripts/fr_add_stacking_es-gn.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-## clears out current annotations first. Questionable?
-cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/bible.es-gn.source.annotated
-
 python3 ./annotate_clwsd.py \
   --annotatedfn ~/terere/bibletools/output/europarl.es-fr.source.annotated \
   --bitext ~/terere/bibletools/output/europarl.es-fr \
