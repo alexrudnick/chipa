2d39c471f32604b88f666b27d438b074653993d8
Alex Rudnick <alex.rudnick@gmail.com>
2018-02-11 09:54:17 -0800

    script for adding es-de annotations for Guarani

---
+++ src/scripts/de_add_stacking_es-gn.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 ./annotate_clwsd.py \
+  --annotatedfn ~/terere/bibletools/output/europarl.es-de.source.annotated \
+  --bitext ~/terere/bibletools/output/europarl.es-de \
+  --alignfn ~/terere/bibletools/output/europarl.es-en.align \
+  --featurefn featuresets/default.features \
+  --annotated_to_classify annotated/bible.es-gn.source.annotated \
+  --featureprefix stack_default_esde \
+
+cat PROBLEMS
