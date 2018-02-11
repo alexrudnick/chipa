274f7a9f414f3ab47a9c0ac353e4f33b03764ce4
Alex Rudnick <alex.rudnick@gmail.com>
2018-02-11 10:03:31 -0800

    use the right alignment file, very important

--- src/scripts/de_add_stacking_es-gn.sh
+++ src/scripts/de_add_stacking_es-gn.sh
@@ -3,7 +3,7 @@
 python3 ./annotate_clwsd.py \
   --annotatedfn ~/terere/bibletools/output/europarl.es-de.source.annotated \
   --bitext ~/terere/bibletools/output/europarl.es-de \
-  --alignfn ~/terere/bibletools/output/europarl.es-en.align \
+  --alignfn ~/terere/bibletools/output/europarl.es-de.align \
   --featurefn featuresets/default.features \
   --annotated_to_classify annotated/bible.es-gn.source.annotated \
   --featureprefix stack_default_esde \
