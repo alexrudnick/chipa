858a43c40e5ca1dc75d4b78cbbcca0373f52da22
Alex Rudnick <alex.rudnick@gmail.com>
2018-02-27 23:08:01 -0800

    scripts to add stacking annotations to es-qu training data

---
+++ src/scripts/de_add_stacking-es-qu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 ./annotate_clwsd.py \
+  --annotatedfn ~/terere/bibletools/output/europarl.es-de.source.annotated \
+  --bitext ~/terere/bibletools/output/europarl.es-de \
+  --alignfn ~/terere/bibletools/output/europarl.es-de.align \
+  --featurefn featuresets/default.features \
+  --annotated_to_classify annotated/bible.es-qu.source.annotated \
+  --featureprefix stack_default_esde \
+
+cat PROBLEMS
