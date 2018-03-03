5730364103a629e95523ce401edb1cbe7c5fc95a
Alex Rudnick <alex.rudnick@gmail.com>
2018-03-02 22:06:06 -0800

    make add_syntactic_es-gn.sh add syntactic annotations to current 'annotated' file rather than overwriting existing file

--- src/scripts/add_syntactic_es-gn.sh
+++ src/scripts/add_syntactic_es-gn.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-cp ~/terere/bibletools/output/bible.es-gn.source.annotated annotated/tmp.annotated
+# cp ~/terere/bibletools/output/bible.es-gn.source.annotated
+#    annotated/tmp.annotated
+cp annotated/bible.es-gn.source.annotated annotated/tmp.annotated
+
+python3 annotated2conllx.py --annotatedfn annotated/tmp.annotated \
+  > ~/chipa/src/annotated/bible.es-gn.source.conll
+
+pushd ~/chipa/maltparser/espmalt
+java -Xmx4096m -jar ~/chipa/maltparser/maltparser-1.9.0/maltparser-1.9.0.jar \
+  -c espmalt-1.0 -i ~/chipa/src/annotated/bible.es-gn.source.conll \
+  -o ~/chipa/src/annotated/bible.es-gn.source.conll.parsed -m parse
+popd
 
 python3 ./annotate_syntactic.py \
   --annotatedfn annotated/tmp.annotated \
   --conllfn ~/chipa/src/annotated/bible.es-gn.source.conll.parsed \
   > annotated/bible.es-gn.source.annotated
 
-rm -f annotated/tmp.annotated
+rm annotated/tmp.annotated
